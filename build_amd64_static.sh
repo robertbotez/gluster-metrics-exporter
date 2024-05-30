3b8fcaa3591ca41884478c2084b904293ed7de32
robertbotez <robert.botez1996@gmail.com>
2024-05-30 20:22:58 +0300

    update crystal version and glustercrystal-cli commit

--- build_amd64_static.sh
+++ build_amd64_static.sh
@@ -7,4 +7,4 @@ VERSION=${VERSION} time -v shards build --static --release --stats --time
 mv bin/gluster-metrics-exporter bin/gluster-metrics-exporter-amd64
 "
 
-docker run --rm -it -v $PWD:/workspace -w /workspace crystallang/crystal:1.1.1-alpine /bin/sh -c "$CMDS"
+docker run --rm -it -v $PWD:/workspace -w /workspace crystallang/crystal:1.12.1-alpine /bin/sh -c "$CMDS"
